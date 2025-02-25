e8f528f63830e2ae6141f76057f2bfd2067dc2d9
Mitzi <yriano69@gmail.com>
2025-02-25 11:07:26 -0600

    chore(homework): add homework

--- fundamentals/index.js
+++ fundamentals/index.js
@@ -1,1 +1,11 @@
 console.log("Hello Wold")
+
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ')
+
+console.log(`Hey there, ${name}!`);
+
+
+const n = prompt('Give me a number ')
+
+console.log(`Number: ${n}`);
