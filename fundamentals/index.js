d2d0564c73cf739bd54fa90dad734f8f14a14c90
Mitzi <yriano69@gmail.com>
2025-03-08 22:50:11 -0600

    feat(operators) add folder structure

--- fundamentals/index.js
+++ fundamentals/index.js
@@ -1,3 +1,4 @@
+/*
 console.log("Hello Wold")
 
 const prompt = require('prompt-sync')();
@@ -9,3 +10,22 @@ console.log(`Hey there, ${name}!`);
 const n = prompt('Give me a number ')
 
 console.log(`Number: ${n}`);
+*/
+/*
+var test // definir
+console.log(test);
+var programName = "DevLaunch" //Inicializar
+console.log(programName); //Acceder
+
+programName = "DevLaunch 2.0" //sobre-escribir
+
+console.log(programName); */
+
+
+var salary = 10000
+var taxes = 3000
+
+salary = salary - taxes
+
+console.log(salary);
+
