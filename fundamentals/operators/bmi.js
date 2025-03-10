582e8ec03f82b34c1388f39dcbb3cb12a66d0ff2
Mitzi <yriano69@gmail.com>
2025-03-09 23:44:44 -0600

    chore(operators) add bmi exercise

--- fundamentals/operators/bmi.js
+++ fundamentals/operators/bmi.js
@@ -1,1 +1,17 @@
 
+const prompt = require('prompt-sync')()
+
+
+
+const name = prompt('Nombre : ')
+const height = parseFloat(prompt('Height : '))
+const weight = parseFloat(prompt('Weight : '))
+
+const imc = weight / Math.pow(height, 2)
+
+console.log(`
+    Name: ${name}
+    Height: ${height}
+    Weight: ${weight}
+    IMC: ${imc}
+    `);
