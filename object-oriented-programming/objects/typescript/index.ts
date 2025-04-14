7294b9cb9868c0fa2f565963b99733a1884da83f
Mitzi <yriano69@gmail.com>
2025-04-13 23:40:40 -0600

    feat(classes): add folder stucture

--- object-oriented-programming/objects/typescript/index.ts
+++ object-oriented-programming/objects/typescript/index.ts
@@ -1,3 +1,30 @@
-console.log("Hello Mitz");
+/*
+class Persona {
+    name: string;
 
-import as* Prompt from 'prompt-sync'
+    constructor(name: string) {
+        this.name = name
+    }
+    saludar = () => {
+        console.log(`Hello, ${this.name} `);
+
+    }
+
+
+}
+
+const persona = new Persona('Mitz')
+persona.saludar()
+
+*/
+
+class Calculadora {
+
+    Suma = (numeroA: number, numeroB: number, numeroC:number) =>{
+        numeroA + numeroB; numeroC
+     
+    }
+}
+
+const Suma = new Suma(8,10);
+Calculadora.Suma()
